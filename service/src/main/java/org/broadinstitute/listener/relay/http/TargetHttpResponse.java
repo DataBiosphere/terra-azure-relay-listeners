3918e3c0fa10011cc5e8a9efd8046267571de33b
Qi Wang <qi@broadinstitute.org>
2022-05-24 15:26:42 -0400

    add CORS headers

--- service/src/main/java/org/broadinstitute/listener/relay/http/TargetHttpResponse.java
+++ service/src/main/java/org/broadinstitute/listener/relay/http/TargetHttpResponse.java
@@ -9,12 +9,17 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS;
+import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS;
+import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
+import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_MAX_AGE;
+import static com.google.common.net.HttpHeaders.CONTENT_SECURITY_POLICY;
+
 /**
  * Represents a response of the local endpoint that is independent of the HTTP client
  * implementation.
  */
 public class TargetHttpResponse extends HttpMessage {
-
   public int getStatusCode() {
     return statusCode;
   }
@@ -81,8 +86,15 @@ public class TargetHttpResponse extends HttpMessage {
           .forEach(
               (key, value) -> {
                 String headerValue = value.iterator().next();
-                responseHeaders.put(key, headerValue);
+                if (headerValue != ACCESS_CONTROL_ALLOW_ORIGIN && headerValue != CONTENT_SECURITY_POLICY)
+                  responseHeaders.put(key, headerValue);
               });
+
+      responseHeaders.put(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+      responseHeaders.put(ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+      responseHeaders.put(ACCESS_CONTROL_ALLOW_HEADERS, "Authorization, Content-Type, Accept, Origin, X-App-Id");
+      responseHeaders.put(ACCESS_CONTROL_MAX_AGE, "1728000");
+//      responseHeaders.put(CONTENT_SECURITY_POLICY, "*"); TODO: add CSP in the future
     }
 
     InputStream body = (InputStream) clientHttpResponse.body();

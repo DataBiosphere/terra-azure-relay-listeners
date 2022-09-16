7f50b6259ad8e1edd9c24f1d531ce9b6fdae9d8b
Qi Wang <qi@broadinstitute.org>
2022-09-16 09:47:52 -0400

    add CORS headers

--- service/src/main/java/org/broadinstitute/listener/relay/Utils.java
+++ service/src/main/java/org/broadinstitute/listener/relay/Utils.java
@@ -1,10 +1,17 @@
 package org.broadinstitute.listener.relay;
 
+import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS;
+import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS;
+import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS;
+import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
+import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_MAX_AGE;
 import static com.google.common.net.HttpHeaders.AUTHORIZATION;
+import static com.google.common.net.HttpHeaders.CONTENT_SECURITY_POLICY;
 
 import java.net.URI;
 import java.util.Map;
 import java.util.Optional;
+import org.broadinstitute.listener.config.CorsSupportProperties;
 
 public class Utils {
   public static final String TOKEN_NAME = "LeoToken";
@@ -22,5 +29,22 @@ public class Utils {
     var splitted = uri.getPath().split("/");
     if (splitted.length == 3) return splitted[2].toLowerCase().equals(SET_COOKIE_API_PATH);
     else return false;
+  }
+
+  public static void writeCORSHeaders(
+      Map<String, String> responseHeaders,
+      Map<String, String> requestHeaders,
+      CorsSupportProperties corsSupportProperties) {
+    responseHeaders.putIfAbsent(
+        ACCESS_CONTROL_ALLOW_METHODS, corsSupportProperties.preflightMethods());
+
+    responseHeaders.putIfAbsent(
+        ACCESS_CONTROL_ALLOW_ORIGIN, requestHeaders.getOrDefault("Origin", "*"));
+
+    responseHeaders.putIfAbsent(ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+    responseHeaders.putIfAbsent(
+        CONTENT_SECURITY_POLICY, corsSupportProperties.contentSecurityPolicy());
+    responseHeaders.putIfAbsent(ACCESS_CONTROL_ALLOW_HEADERS, corsSupportProperties.allowHeaders());
+    responseHeaders.putIfAbsent(ACCESS_CONTROL_MAX_AGE, corsSupportProperties.maxAge());
   }
 }

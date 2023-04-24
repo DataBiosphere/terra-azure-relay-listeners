7f9eb7ca6371a0ab2535cef42f4de7d43956f10b
Robert Title <rtitle@broadinstitute.org>
2023-04-24 14:54:28 -0400

    Better reactor

--- service/src/main/java/org/broadinstitute/listener/relay/transport/RelayedRequestPipeline.java
+++ service/src/main/java/org/broadinstitute/listener/relay/transport/RelayedRequestPipeline.java
@@ -4,6 +4,8 @@ import com.microsoft.azure.relay.HybridConnectionChannel;
 import com.microsoft.azure.relay.RelayedHttpListenerContext;
 import org.broadinstitute.listener.relay.http.ListenerConnectionHandler;
 import org.broadinstitute.listener.relay.http.RelayedHttpRequestProcessor;
+import org.broadinstitute.listener.relay.http.RelayedHttpRequestProcessor.Result;
+import org.broadinstitute.listener.relay.http.TargetHttpResponse;
 import org.broadinstitute.listener.relay.wss.ConnectionsPair;
 import org.broadinstitute.listener.relay.wss.WebSocketConnectionsHandler;
 import org.broadinstitute.listener.relay.wss.WebSocketConnectionsRelayerService;
@@ -91,12 +93,19 @@ public class RelayedRequestPipeline {
                             return httpRequestProcessor.executeRequestOnTarget(c);
                           }
                           httpRequestProcessor.writeNotAcceptedResponseOnCaller(c);
-                          return null;
+                          return Mono.empty();
                         })
                     .subscribeOn(scheduler))
         .flatMap(
             (r) ->
-                Mono.fromCallable(() -> httpRequestProcessor.writeTargetResponseOnCaller(r))
+                Mono.fromCallable(
+                        () -> {
+                          if (r instanceof TargetHttpResponse) {
+                            return httpRequestProcessor.writeTargetResponseOnCaller(
+                                (TargetHttpResponse) r);
+                          }
+                          return Result.FAILURE;
+                        })
                     .subscribeOn(scheduler))
         .doOnError(ex -> logger.error("Failed to process the request.", ex))
         .subscribe(

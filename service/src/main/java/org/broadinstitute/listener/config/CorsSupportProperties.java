a782afefca012e1de6f5de99944dc98398c210ae
lmcnatt <85642387+lucymcnatt@users.noreply.github.com>
2023-06-13 10:15:42 -0400

    Revert "Revert "Changed response header to always be wildcard. (#29)" (#36)" (#37)
    This reverts commit 13b7af70ba8136505ffee2e3e98d706c2ca798b3.

--- service/src/main/java/org/broadinstitute/listener/config/CorsSupportProperties.java
+++ service/src/main/java/org/broadinstitute/listener/config/CorsSupportProperties.java
@@ -1,4 +1,10 @@
 package org.broadinstitute.listener.config;
 
+import java.util.List;
+
 public record CorsSupportProperties(
-    String preflightMethods, String allowHeaders, String maxAge, String contentSecurityPolicy) {}
+    String preflightMethods,
+    String allowHeaders,
+    String maxAge,
+    String contentSecurityPolicy,
+    List<String> validHosts) {}

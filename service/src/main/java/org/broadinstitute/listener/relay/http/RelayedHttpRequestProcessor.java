f53579a20179e150c3b909c4e4508bdcc222cae1
Mneme Nolting <mneme@broadinstitute.org>
2023-06-20 15:15:41 -0400

    [IA-4395] Remove HttpOnly flag in cookie responses. NOTE unsafe (rolls back security compliance changes from IA-4082)

--- service/src/main/java/org/broadinstitute/listener/relay/http/RelayedHttpRequestProcessor.java
+++ service/src/main/java/org/broadinstitute/listener/relay/http/RelayedHttpRequestProcessor.java
@@ -169,7 +169,7 @@ public class RelayedHttpRequestProcessor {
             .put(
                 SET_COOKIE,
                 String.format(
-                    "%s=%s; Max-Age=%s; Path=/; Secure; SameSite=None; HttpOnly",
+                    "%s=%s; Max-Age=%s; Path=/; Secure; SameSite=None;",
                     Utils.TOKEN_NAME, authToken.get(), expiresIn.orElse(0L)));
 
         Utils.writeCORSHeaders(

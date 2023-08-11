b5a2e7ad503325d8d389dadeaf0b09789552df50
Saloni Shah <salonipshah11@gmail.com>
2023-08-11 13:52:56 -0400

    make it boolean

--- service/src/main/java/org/broadinstitute/listener/relay/inspectors/SamResourceClient.java
+++ service/src/main/java/org/broadinstitute/listener/relay/inspectors/SamResourceClient.java
@@ -42,7 +42,7 @@ public class SamResourceClient {
         var resourceApi = new ResourcesApi(samClient);
 
         // check that user has access to workspace
-        var workspaceAccess = resourceApi.resourcePermissionV2("workspace", samResourceId, "read");
+        boolean workspaceAccess = resourceApi.resourcePermissionV2("workspace", samResourceId, "read");
         if (!workspaceAccess) {
           logger.error("Unauthorized request. User doesn't have access to workspace");
           return Instant.EPOCH;
